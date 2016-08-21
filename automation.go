02092082bc1d53104f468d6665dbb6d89a497ed6
tfindelkind <thomas@thomas-findelkind.de>
2016-08-21 15:25:43 +0200

    empty automation.go

--- automation.go
+++ automation.go
@@ -1,40 +1,9 @@
 package main
 
-
-import(
-
-    "os/exec"
-    "runtime"
-    "fmt"
-    "log"
-    
-)
+// just a placeholder to avoid errors when auto build/install 
 
 func main(){    
 	
-	fmt.Println("Installing automation ....")
 	
-	switch os := runtime.GOOS; os {
-	case "windows":
-		exec.Command("cmd", "/C", "install.bat")
-    case "linux":
-    	{ 
-		 out, err := exec.Command("/bin/bash","install").Output()
-		 if err != nil {
-		 log.Fatal(err)
-		 }
-		 fmt.Println(string(out))
-		}		
-	default:
-     { 
-			out, err := exec.Command("/bin/bash","install").Output()
-			if err != nil {
-			log.Fatal(err)
-			}
-			fmt.Println(string(out))
-	 }	
-	}
-	
-	fmt.Println("Installing ended ....") 	       
 }
 
