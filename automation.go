3741a57518ed6bb05f46e9a5f39b072353ca08a3
tfindelkind <thomas@thomas-findelkind.de>
2016-08-21 14:54:20 +0200

    install added

---
+++ automation.go
@@ -0,0 +1,40 @@
+package main
+
+
+import(
+
+    "os/exec"
+    "runtime"
+    "fmt"
+    "log"
+    
+)
+
+func main(){    
+	
+	fmt.Println("Installing automation ....")
+	
+	switch os := runtime.GOOS; os {
+	case "windows":
+		exec.Command("cmd", "/C", "install")
+    case "linux":
+    	{ 
+		 out, err := exec.Command("/bin/bash","install").Output()
+		 if err != nil {
+		 log.Fatal(err)
+		 }
+		 fmt.Println(string(out))
+		}		
+	default:
+     { 
+			out, err := exec.Command("/bin/bash","install").Output()
+			if err != nil {
+			log.Fatal(err)
+			}
+			fmt.Println(string(out))
+	 }	
+	}
+	
+	fmt.Println("Installing ended ....") 	       
+}
+
