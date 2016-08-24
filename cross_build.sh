783b5a02b091b72ca937f1b3207b5cb3f3a7aaba
tfindelkind <thomas@thomas-findelkind.de>
2016-08-24 18:54:27 +0200

    added cross compile script for binaries

--- cross_build.sh
+++ cross_build.sh
@@ -1,2 +1,2 @@
-env GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/deploy_cloud_vm github.com/Tfindelkind/automation/deploy_cloud_vm 
-env GOOS=linux GOARCH=amd64 go build -o ./bin/linux/deploy_cloud_vm github.com/Tfindelkind/automation/deploy_cloud_vm 
+env GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/deploy_cloud_vm_mac github.com/Tfindelkind/automation/deploy_cloud_vm 
+env GOOS=linux GOARCH=amd64 go build -o ./bin/linux/deploy_cloud_vm_linux github.com/Tfindelkind/automation/deploy_cloud_vm 
