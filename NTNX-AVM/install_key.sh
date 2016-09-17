ce114f56ba4316416d5f11b7fdb9aca9647d2d5e
tfindelkind <thomas@thomas-findelkind.de>
2016-09-17 20:38:07 +0200

    key install

--- NTNX-AVM/install_key.sh
+++ NTNX-AVM/install_key.sh
@@ -23,7 +23,7 @@
 
 ssh-keygen -b 2048 -t rsa -f /home/nutanix/.ssh/id_rsa -q -N ""
 
-ncli -s 192.168.178.130 -u admin -p nutanix/4u cluster status | grep Name | cut -d':' -f2 > cvm_list
+ncli -s 192.168.178.130 -u admin -p nutanix/4u cluster status | grep Name | cut -d':' -f2 | tr -d ' ' > cvm_list
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
        scp /home/nutanix/.ssh/id_rsa.pub nutanix@$line:/home/nutanix/
