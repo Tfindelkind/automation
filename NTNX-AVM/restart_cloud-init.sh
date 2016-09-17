1a7117ffdaad7bd0ce2ab901f0cc3cc5d4df04e2
tfindelkind <thomas@thomas-findelkind.de>
2016-09-17 18:29:45 +0200

    install NTNX-AVM tools

---
+++ NTNX-AVM/restart_cloud-init.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 Thomas Findelkind
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# MORE ABOUT THIS SCRIPT AVAILABLE IN THE README AND AT:
+#
+# http://tfindelkind.com
+#
+# ---------------------------------------------------------------------------- #
+
+## MAIN block-------------------------------------------------------------
+
+# retrieve os
+case $OSTYPE in
+	darwin* )
+		os="mac" ;;
+	linux* )
+		if [ -f "/etc/redhat-release" ]; then os="redhat"; fi
+		if [ -f "/etc/debian_version" ]; then os="debian"; fi
+		;;
+esac
+
+## redhat style
+ if [ $os == "redhat" ]; then
+  # remove the instance
+  sudo rm -rf /var/lib/cloud/instances
+  # run the cloud-init again
+  sudo systemctl restart cloud-init.service
+ fi
+
+ ## debian style
+ if [ $os == "debian" ]; then
+	# remove the instance
+  sudo rm -rf /var/lib/cloud/instances
+ 	# run the cloud-init again
+  sudo systemctl restart cloud-init.service
+ fi
