5d7f221f960d059afdf2849f9d90f6e8a06b9a9f
tfindelkind <thomas@thomas-findelkind.de>
2016-09-17 20:12:28 +0200

    key install

--- NTNX-AVM/install_key.sh
+++ NTNX-AVM/install_key.sh
@@ -1,9 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 Thomas Findelkind
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# MORE ABOUT THIS SCRIPT AVAILABLE IN THE README AND AT:
+#
+# http://tfindelkind.com
+#
+# ---------------------------------------------------------------------------- #
+
 ssh-keygen -b 2048 -t rsa -f /home/nutanix/.ssh/id_rsa -q -N ""
 
 ncli -s 192.168.178.130 -u admin -p nutanix/4u ms ls | grep Name | cut -d':' -f2 > cvm_list
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    if [ $line != "root" ]
-    scp id_rsa.pub nutanix@$line:/home/nutanix/
-    ssh nutanix@$line cat id_rsa.pub >> .ssh/authorized_keys2
+    if [ $line != "root" ]; then
+     scp id_rsa.pub nutanix@$line:/home/nutanix/
+     ssh nutanix@$line cat id_rsa.pub >> .ssh/authorized_keys2
+   fi
 done < cvm_list
