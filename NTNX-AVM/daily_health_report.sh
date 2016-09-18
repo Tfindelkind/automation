a5dbabeb471eb6e6580acf61620b376e4c80fae5
tfindelkind <thomas@thomas-findelkind.de>
2016-09-18 13:11:57 +0200

    daily health

--- NTNX-AVM/daily_health_report.sh
+++ NTNX-AVM/daily_health_report.sh
@@ -1,8 +1,8 @@
 name=$(date '+%y-%m-%d')
 
-ssh nutanix@192.168.178.130 '/usr/local/nutanix/apache-cassandra/bin/nodetool -h 0 ring > daily_health-report-$name.txt' < /dev/null
-ssh nutanix@192.168.178.130 '/usr/local/nutanix/cluster/bin/genesis status >> daily_health-report-$name.txt' < /dev/null
-ssh nutanix@192.168.178.130 '/usr/local/nutanix/cluster/bin/cluster status >> daily_health-report-$name.txt' < /dev/null
-ssh nutanix@192.168.178.130 'export PS1="fake>" ; source /etc/profile ; df -h >> daily_health-report-$name.txt' < /dev/null
-ssh nutanix@192.168.178.130 '~/prism/cli/ncli alerts ls >> daily_health-report-$name.txt' < /dev/null
-ssh nutanix@192.168.178.130 'export PS1="fake>" ; source /etc/profile ; __allssh "ls -lahrt ~/data/logs | grep -i fatal" >> daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.132 'export PS1="fake>" ; source /etc/profile ; nodetool -h 0 ring > daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.132 'export PS1="fake>" ; source /etc/profile ; genesis status >> daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.132 'export PS1="fake>" ; source /etc/profile ; cluster status >> daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.132 'export PS1="fake>" ; source /etc/profile ; df -h >> daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.132 'export PS1="fake>" ; source /etc/profile ; ncli alerts ls >> daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.132 'export PS1="fake>" ; source /etc/profile ; __allssh "ls -lahrt ~/data/logs | grep -i fatal" >> daily_health-report-$name.txt' < /dev/null
