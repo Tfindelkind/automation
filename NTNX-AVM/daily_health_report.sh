69a14098a774541321cf45267f184adeb9b1acc6
tfindelkind <thomas@thomas-findelkind.de>
2016-09-18 12:04:36 +0200

    daily_health_report

--- NTNX-AVM/daily_health_report.sh
+++ NTNX-AVM/daily_health_report.sh
@@ -1,7 +1,8 @@
+name=$(date '+%y-%m-%d')
 
-ssh nutanix@192.168.178.130 'nodetool -h 0 ring'
-ssh root@192.168.1.1 'genesis status'
-ssh root@192.168.1.1 'cluster status'
-ssh root@192.168.1.1 'uptime' allssh df -h
-ssh root@192.168.1.1 'uptime' ncli alerts ls
-ssh root@192.168.1.1 'uptime' allssh "ls -lahrt ~/data/logs | grep -i fatal"
+ssh nutanix@192.168.178.130 'nodetool -h 0 ring > daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.130 'genesis status >> daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.130 'cluster status >> daily_health-report-$name.txt' < /dev/null
+ssh nutanix@192.168.178.130 'allssh df -h >> daily_health-report-$name.txt' < /dev/null
+ssh root@192.168.1.1 'ncli alerts ls >> daily_health-report-$name.txt' < /dev/null
+ssh root@192.168.1.1 'allssh "ls -lahrt ~/data/logs | grep -i fatal" >> daily_health-report-$name.txt' < /dev/null
