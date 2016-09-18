0a240ed5e5870ad96374328c0bb8e7dc9f9e5c41
tfindelkind <thomas@thomas-findelkind.de>
2016-09-18 13:18:19 +0200

    daily health

--- NTNX-AVM/daily_health_report.sh
+++ NTNX-AVM/daily_health_report.sh
@@ -1,6 +1,6 @@
 name=$(date '+%y-%m-%d')
 
-ssh nutanix@192.168.178.132 "export PS1="fake>" ; source /etc/profile ; nodetool -h 0 ring > daily_health-report-$name.txt" < /dev/null
+ssh nutanix@192.168.178.132 "export PS1='fake>' ; source /etc/profile ; nodetool -h 0 ring > daily_health-report-$name.txt" < /dev/null
 ssh nutanix@192.168.178.132 "export PS1="fake>" ; source /etc/profile ; genesis status >> daily_health-report-$name.txt" < /dev/null
 ssh nutanix@192.168.178.132 "export PS1="fake>" ; source /etc/profile ; cluster status >> daily_health-report-$name.txt" < /dev/null
 ssh nutanix@192.168.178.132 "export PS1="fake>" ; source /etc/profile ; df -h >> daily_health-report-$name.txt" < /dev/null
